8ba93a45e8e091601403dc680e1f79c1a7cd5967
KaiHikichi <kai.hikichi@gmail.com>
2026-03-07 15:38:42 -0800

    read saved songs from user

--- src/shuffle.ts
+++ src/shuffle.ts
@@ -37,6 +37,33 @@ interface SpotifyDeviceObject{
     type: string;
 }
 
+interface SpotifyTrack{
+    album: object;
+    artists: object[];
+    duration: number;
+    external_irls: object;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+}
+
+interface SpotifyTrackList {
+    href: string;
+    limit: number;
+    next: string;
+    offset: number;
+    previous: string;
+    total: number;
+    items: SpotifyTrackObject[];
+}
+
+interface SpotifyTrackObject {
+    added_at: string;
+    track: SpotifyTrack;
+}
+
 
 //route handlers/////////////////////////////////////////////////
 
@@ -102,13 +129,9 @@ app.get('/callback', async function(req, res) {
 app.get('/shuffle', async (req, res) => {
 
     try {
-        let devices: SpotifyDeviceObject[] = await getDevices();
-        if (devices.length == 0){
-            res.send(`No Device Found`);
-            return;
-        }
-
-        res.send(`${devices[0]?.name}`);
+        let allTracks: SpotifyTrack[] = await getAllSavedTracks(req, res);
+        printTracks(allTracks, "out/songs.txt");
+        
 
     } 
     catch (error: unknown) {
@@ -134,31 +157,6 @@ app.get('/shuffle', async (req, res) => {
 
 //helpers////////////////////////////////////////////
 
-function generateRandomString(length: number) {
-  let text = '';
-  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-function handleError(error: unknown, source: string){
-    if (axios.isAxiosError(error)) {
-        if (error.response) {
-            console.error("API call failed:", error.response?.data || error.message);
-        } else if (error.request) {
-            console.error('No response received:', error.request);
-        } else {
-            console.error('Error in request setup:', error.message);
-        }
-    } 
-    else {
-        console.error('An unknown error occurred:', error);
-    } 
-    console.error(`From: ${source}`);
-}
-
 //gets all active instances of Spotify (must be open or playing music)
 async function getDevices(): Promise<SpotifyDeviceObject[]> {
     let devices: SpotifyDeviceObject[] = [];
@@ -177,4 +175,118 @@ async function getDevices(): Promise<SpotifyDeviceObject[]> {
     }
 
     return devices;
+}
+
+//get 50 liked songs ( returns songs either sorted by recently added )
+//offset: integer denoting where to start reading from saved library
+async function getSavedTracks(offset: number): Promise<SpotifyTrack[]> {
+    var trackList: SpotifyTrackList = {
+        href: "",
+        limit: 1,
+        next: "",
+        offset: 1,
+        previous: "",
+        total: 1,
+        items: []
+    } 
+    let tracks: SpotifyTrack[] = [];
+
+    try {
+        const response = await axios.get('https://api.spotify.com/v1/me/tracks', {
+            headers: {
+                Authorization: `Bearer ${access_token}`
+            },
+            params: {
+                limit: 50,
+                offset: offset
+            }
+        });
+
+        trackList = response.data;
+
+        trackList.items.forEach(t => {
+            tracks.push(t.track);
+        });
+
+    } catch (error: unknown) {
+        //handle API rate limits
+        if (axios.isAxiosError(error) && error.response?.status == 429) {
+            const retryAfterHeader = error.response?.headers['retry-after'];
+            const waitSeconds = retryAfterHeader ? parseInt(retryAfterHeader, 10) : NaN;
+            console.warn(`Rate limited. Wait for ${waitSeconds} seconds before retrying get saved tracks.`);
+            await sleep(waitSeconds * 1000);
+            return await getSavedTracks(offset);
+        }
+        else{
+            handleError(error, "Tracks");
+        }
+        
+        
+    }
+    return tracks;
+}
+
+//put all songs in user's liked songs in an array
+async function getAllSavedTracks(req: express.Request, res: express.Response): Promise<SpotifyTrack[]> {
+    let offset = 0;
+    let allTracks: SpotifyTrack[] = [];
+    let tempTracks = await getSavedTracks(offset);
+
+    //push each batch of 50
+    while(tempTracks.length == 50){
+        //push each track to allTracks array
+        tempTracks.forEach(item => {
+            allTracks.push(item);
+        });
+        offset = offset + 50;
+        tempTracks = await getSavedTracks(offset);
+    }
+    //push final batch
+    tempTracks.forEach(item => {
+        allTracks.push(item);
+    });
+
+    return allTracks;
+}
+
+//print tracks to fileName
+function printTracks(tracks: SpotifyTrack[], fileName: string){
+
+    let i = 1
+    fs.writeFileSync(fileName, "Songs:\n");
+    tracks.forEach(item => {
+        fs.appendFileSync(fileName, `${i}: ${item.name}\n`);
+        i++;
+    });
+
+}
+
+function handleError(error: unknown, source: string){
+    if (axios.isAxiosError(error)) {
+        if (error.response) {
+            console.error("API call failed:", error.response?.data || error.message);
+        } else if (error.request) {
+            console.error('No response received:', error.request);
+        } else {
+            console.error('Error in request setup:', error.message);
+        }
+    } 
+    else {
+        console.error('An unknown error occurred:', error);
+    } 
+    console.error(`From: ${source}`);
+}
+
+function generateRandomString(length: number) {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+// pause for ms milliseconds
+function sleep(ms: number): Promise<void> {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
